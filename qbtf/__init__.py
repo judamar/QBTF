449c643d23f81af0f299d18d998226b0bc0a70a2
judamar <juancho.o.mart@gmail.com>
2025-03-02 21:28:11 -0500

    dev branch

--- qbtf/__init__.py
+++ qbtf/__init__.py
@@ -10,7 +10,8 @@ def print_msg(server: PluginServerInterface, msg: str, prefix='[QBTF] '):
 	server.get_server().logger.info(msg)
 	server.get_server().say(msg)
 
-def comprimir_qb(server: PluginServerInterface): #It compresses the source code of the plugin into a zip file.
+#TODO: put the qb_comp in the dest path
+def compress_qb(server: PluginServerInterface): #It compresses the source code of the plugin into a zip file.
 	print_msg(server, "§a[+]§r §3Compressing...§r")
 	try:
 		shutil.make_archive(conf.comp_name, 'zip', conf.source_path)
@@ -18,12 +19,21 @@ def comprimir_qb(server: PluginServerInterface): #It compresses the source code 
 	except:
 		print_msg(server, "§c[-]§r Epic fail")
 
-def subirAFirebase(server: PluginServerInterface): #It uploads the zip file to Firebase Storage
+#TODO: remove slot1 content, and put the qb_comp content in the slot1
+def extract_qb(server: PluginServerInterface): #It extracts the source code of the plugin from the zip file.
+	print_msg(server, "§a[+]§r §3Extracting...§r")
+	try:
+		shutil.unpack_archive(conf.comp_name + conf.extension, conf.source_path, 'zip')
+		print_msg(server, "§a[+]§r DONE")
+	except:
+		print_msg(server, "§c[-]§r Epic fail")
+
+def upload_to_firebase(server: PluginServerInterface): #It uploads the zip file to Firebase Storage
     print_msg(server, "§a[+]§r §3Uploading to Firebase...§r")
     try:
         firebase = pyrebase.initialize_app(conf.firebase_config)
         storage = firebase.storage()
-        file = conf.comp_name + '.zip'
+        file = conf.comp_name + conf.extension
         cloudfilename = (conf.fb_path + file)
         storage.child(cloudfilename).put(file)
         print_msg(server, "§a[+]§r DONE")
@@ -31,11 +41,25 @@ def subirAFirebase(server: PluginServerInterface): #It uploads the zip file to F
         print_msg(server, "§c[-]§r ERROR")
         print_msg(server, "§c[-]§r You must fill the 'firebase_config' field in the plugin configuration file.")
 
+def download_from_firebase(server: PluginServerInterface): #It downloads the zip file from Firebase Storage
+    print_msg(server, "§a[+]§r §3Downloading from Firebase...§r")
+    try:
+        firebase = pyrebase.initialize_app(conf.firebase_config)
+        storage = firebase.storage()
+        file = conf.comp_name + conf.extension
+        cloudfilename = (conf.fb_path + file)
+        storage.child(cloudfilename).download(file)
+        print_msg(server, "§a[+]§r DONE")
+    except KeyError:
+        print_msg(server, "§c[-]§r ERROR")
+        print_msg(server, "§c[-]§r You must fill the 'firebase_config' field in the plugin configuration file.")
+
 @new_thread("QBTF execution")
 def execute(server: PluginServerInterface): #Executes 'comprimir_qb' and 'subirAFirebase' functions
-	comprimir_qb(server)
-	subirAFirebase(server)
+	compress_qb(server)
+	upload_to_firebase(server)
 
+#TODO: add !!qbtf upload and !!qbtf download commands using .then()
 def on_load(server: PluginServerInterface, old_module):
 	global conf #do conf global
 	conf = server.load_config_simple('config.json', target_class=Configure) #load config.json file to conf
