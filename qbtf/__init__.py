7f77c4acc747a1c5a5fa8fbf0cccbe7b9e8c1c5a
judamar <juancho.o.mart@gmail.com>
2025-03-06 22:51:24 -0500

    refactor(upload): improve upload and download functions, enhance logging and error messages

--- qbtf/__init__.py
+++ qbtf/__init__.py
@@ -10,95 +10,94 @@ out_path = conf.dest_path+conf.comp_name #It defines the path where the zip file
 unknown_argument_msg = gen_unknown_argument_message()
 
 def print_msg(server: PluginServerInterface, msg: str, prefix='[QBTF] '):
-	msg = RTextList(prefix, msg)
-	server.get_server().logger.info(msg)
-	server.get_server().say(msg)
+    msg = RTextList(prefix, msg)
+    server.logger.info(msg)
+    server.say(msg)
 
+@new_thread("QBTF Check Folder")
 def check_folder(server: PluginServerInterface): #It checks if the folder exists, if not, it creates it.
-	if not os.path.exists(conf.dest_path):
-		os.makedirs(conf.dest_path)
-		server.logger.info("dest_path folder created")
+    if not os.path.exists(conf.dest_path):
+        os.makedirs(conf.dest_path)
+        server.logger.info("dest_path folder created")
 
 def compress_qb(server: PluginServerInterface): #It compresses the source code of the plugin into a zip file.
-	print_msg(server, "§a[+]§r §3Compressing...§r")
-	try:
-		shutil.make_archive(out_path, conf.extension, conf.source_path)
-		print_msg(server, "§a[+]§r DONE")
-	except Exception as error:
-		print_msg(server, "§c[-]§r Epic fail")
-		server.get_server().logger.exception("ERROR while compressing the plugin")
+    print_msg(server, f"§a[+]§r Compressing {conf.source_path}")
+    try:
+        shutil.make_archive(out_path, conf.extension, conf.source_path)
+        print_msg(server, "§a[+]§r Success§r")
+    except Exception as error:
+        print_msg(server, f"§c[-] Compression failed§r {error}")
 
-#TODO: remove slot1 content, and put the qb_comp content in the slot1
 def extract_qb(server: PluginServerInterface): #It extracts the source code of the plugin from the zip file.
-	print_msg(server, "§a[+]§r §3Extracting...§r")
-	try:
-		shutil.unpack_archive(conf.comp_name + conf.extension, conf.source_path, conf.extension)
-		print_msg(server, "§a[+]§r DONE")
-	except:
-		print_msg(server, "§c[-]§r Epic fail")
-
+    print_msg(server, f"§a[+]§r Extracting")
+    try:
+        shutil.rmtree(conf.source_path)
+        os.makedirs(conf.source_path)
+        shutil.unpack_archive(conf.dest_path + conf.comp_name + '.' + conf.extension, conf.source_path, conf.extension)
+        os.remove(out_path +"."+ conf.extension)
+        print_msg(server, "§a[+]§r Success")
+        print_msg(server, f"§a[+]§r Ready to restore the {conf.source_path}")
+    except Exception as error:
+        print_msg(server, f"§c[-] Extraction failed§r {error}")
 
 def upload_to_firebase(server: PluginServerInterface): #It uploads the zip file to Firebase Storage
-    print_msg(server, "§a[+]§r §3Uploading to Firebase...§r")
+    print_msg(server, f"§a[+]§r Uploading to Firebase {conf.fb_path}§r")
     try:
         firebase = pyrebase.initialize_app(conf.firebase_config)
         storage = firebase.storage()
         file = out_path +"."+ conf.extension
-        server.get_server().logger.info(file)
         cloudfilename = (conf.fb_path + conf.comp_name +"."+ conf.extension)
-        server.get_server().logger.info(cloudfilename)
         storage.child(cloudfilename).put(file)
         os.remove(file)
-        print_msg(server, "§a[+]§r DONE")
-    except KeyError:
-        print_msg(server, "§c[-]§r ERROR")
-        print_msg(server, "§c[-]§r You must fill the 'firebase_config' field in the plugin configuration file.")
+        print_msg(server, "§a[+]§r Success")
+    except Exception as error:
+        print_msg(server, f"§c[-] Uploading failed§r {error}")
 
 def download_from_firebase(server: PluginServerInterface): #It downloads the zip file from Firebase Storage
-    print_msg(server, "§a[+]§r §3Downloading from Firebase...§r")
+    print_msg(server, f"§a[+]§r Downloading from Firebase {conf.fb_path}§r")
     try:
         firebase = pyrebase.initialize_app(conf.firebase_config)
         storage = firebase.storage()
-        file = conf.comp_name + conf.extension
-        cloudfilename = (conf.fb_path + file)
-        storage.child(cloudfilename).download(file)
-        print_msg(server, "§a[+]§r DONE")
-    except KeyError:
-        print_msg(server, "§c[-]§r ERROR")
-        print_msg(server, "§c[-]§r You must fill the 'firebase_config' field in the plugin configuration file.")
+        file = conf.comp_name +"."+ conf.extension
+        cloudfilename = (conf.fb_path + conf.comp_name +"."+ conf.extension)
+        storage.child(cloudfilename).download(conf.dest_path, conf.dest_path+file)
+        print_msg(server, "§a[+]§r Success")
+    except Exception as error:
+        print_msg(server, f"§c[-] ERROR§r {error}")
 
-def upload():
-  compress_qb()
-  upload_to_firebase()
+@new_thread("QBTF Upload")
+def upload(server: PluginServerInterface):
+    compress_qb(server)
+    upload_to_firebase(server)
 
-def download():
-  download_from_firebase()
-  extract_qb()
+@new_thread("QBTF Download")
+def download(server: PluginServerInterface):
+    download_from_firebase(server)
+    extract_qb(server)
 
 #|REGISTER COMMANDS|    
 def register_command(server: PluginServerInterface):
-  def get_literal_node(literal):
-    lvl = conf.permission
-    return Literal(literal).requires(lambda src: src.has_permission(lvl)).on_error(RequirementNotMet, lambda src: src.reply("Permission denied"), handled=True)
+    def get_literal_node(literal):
+        lvl = conf.permission
+        return Literal(literal).requires(lambda src: src.has_permission(lvl)).on_error(RequirementNotMet, lambda src: src.reply("Permission denied"), handled=True)
 
-  server.register_command(
-    Literal(conf.prefix).
-      runs(lambda src: src.reply(gen_help_message())).
-        on_error(UnknownArgument, lambda src, _: print_msg(server, unknown_argument_msg), handled=True).
-          then(
-            get_literal_node('upload').runs(lambda src, ctx: upload())
-            ).
-          then(
-            get_literal_node('download').runs(lambda src, ctx: download())
-          )
+    server.register_command(
+        Literal(conf.prefix).
+            runs(lambda src: src.reply(gen_help_message())).
+                on_error(UnknownArgument, lambda src, _: print_msg(server, unknown_argument_msg), handled=True).
+            then(
+                get_literal_node('upload').runs(lambda src, ctx: upload(src.get_server()))
+                ).
+            then(
+                get_literal_node('download').runs(lambda src, ctx: download(src.get_server()))
+            )
     )
 
-#TODO: add !!qbtf upload and !!qbtf download commands using .then()
 def on_load(server: PluginServerInterface, old_module):
-  global conf #do conf global
-  check_folder(server) #check folder
-  conf = server.load_config_simple('config.json', target_class=Configure) #load config.json file to conf
-  msg = 'Plugin qbtf loaded, use {}'.format(conf.prefix) #message showed when server start
-  server.logger.info(msg) #displays message
-  register_command(server) #register command
-  server.register_help_message(conf.prefix, {'en_us': help_message}) # when !!help it shows help message
+    global conf #do conf global
+    check_folder(server) #check folder
+    conf = server.load_config_simple('config.json', target_class=Configure) #load config.json file to conf
+    msg = 'Plugin qbtf loaded, use {}'.format(conf.prefix) #message showed when server start
+    server.logger.info(msg) #displays message
+    register_command(server) #register command
+    server.register_help_message(conf.prefix, {'en_us': help_message}) # when !!help it shows help message
